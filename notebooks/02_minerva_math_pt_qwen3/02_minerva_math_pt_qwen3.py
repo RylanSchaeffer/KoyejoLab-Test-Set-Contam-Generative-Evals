397918a5a2ae8fe03966fe7451cf5930a2cf3111
Rylan <rylanschaeffer@gmail.com>
2025-08-24 09:41:27 -0700

    Prototyping 02 minerval math PT Qwen 3 notebook.

---
+++ notebooks/02_minerva_math_pt_qwen3/02_minerva_math_pt_qwen3.py
@@ -0,0 +1,120 @@
+import ast
+
+import matplotlib.colors
+import matplotlib.pyplot as plt
+import matplotlib.ticker
+import matplotlib.transforms
+import numpy as np
+import os
+import pandas as pd
+import seaborn as sns
+import wandb
+
+import src.analyze
+import src.globals
+import src.plot
+
+refresh = False
+# refresh = True
+
+data_dir, results_dir = src.analyze.setup_notebook_dir(
+    notebook_dir=os.path.dirname(os.path.abspath(__file__)),
+    refresh=False,
+)
+
+
+sweep_ids = [
+    "ofo96w3y",  # Qwen 3 34M    Train: Finished     Eval: Finished
+    "ynkr8xz6",  # Qwen 3 93M    Train: Finished     Eval: Finished
+]
+
+pretrain_run_configs_df: pd.DataFrame = src.analyze.download_wandb_project_runs_configs(
+    wandb_project_path="memorization-scoring-vs-sampling-pt",
+    data_dir=data_dir,
+    sweep_ids=sweep_ids,
+    refresh=refresh,
+    wandb_username=wandb.api.default_entity,
+    finished_only=True,
+)
+pretrain_run_configs_df["Num. Replicas Per Epoch"] = pretrain_run_configs_df[
+    "data_config"
+].apply(lambda x: ast.literal_eval(x)["num_benchmark_replicas_per_epoch"])
+pretrain_run_configs_df["Num. Epochs"] = pretrain_run_configs_df[
+    "trainer_config"
+].apply(lambda x: ast.literal_eval(x)["num_train_epochs"])
+pretrain_run_configs_df["Num. Replicas"] = (
+    pretrain_run_configs_df["Num. Replicas Per Epoch"]
+    * pretrain_run_configs_df["Num. Epochs"]
+)
+pretrain_run_configs_df["Num. Parameters"] = pretrain_run_configs_df[
+    "model/num_parameters"
+]
+pretrain_run_configs_df["Num. Tokens"] = (
+    20
+    * pretrain_run_configs_df["Num. Parameters"]
+    * pretrain_run_configs_df["trainer_config"].apply(
+        lambda x: ast.literal_eval(x)["overtrain_multiplier"]
+    )
+)
+pretrain_run_configs_df["FLOP (6ND)"] = (
+    6
+    * pretrain_run_configs_df["Num. Parameters"]
+    * pretrain_run_configs_df["Num. Tokens"]
+)
+
+plt.close()
+plt.figure(figsize=(8, 6))
+g = sns.scatterplot(
+    data=pretrain_run_configs_df,
+    x="FLOP (6ND)",
+    y="eval/loss",
+    hue="Num. Replicas",
+    hue_norm=matplotlib.colors.SymLogNorm(linthresh=1),
+    palette="viridis",
+)
+g.set(xscale="log", yscale="log", ylabel="Cross Entropy on MATH")
+sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
+src.plot.save_plot_with_multiple_extensions(
+    plot_dir=results_dir,
+    plot_filename="y=loss_x=flop_hue=num_replicas",
+)
+plt.show()
+
+plt.close()
+plt.figure(figsize=(8, 6))
+g = sns.scatterplot(
+    data=pretrain_run_configs_df,
+    x="Num. Replicas",
+    y="eval/loss",
+    hue="FLOP (6ND)",
+    hue_norm=matplotlib.colors.LogNorm(),
+    palette="magma",
+    legend=False,
+)
+
+g.set(
+    xscale="symlog",
+    xlim=(-0.1, 1000),
+    yscale="log",
+    ylabel="Cross Entropy on MATH",
+)
+
+# Move the legend into a colorbar and apply scientific notation
+norm = matplotlib.colors.LogNorm(
+    vmin=pretrain_run_configs_df["FLOP (6ND)"].min(),
+    vmax=pretrain_run_configs_df["FLOP (6ND)"].max(),
+)
+sm = matplotlib.cm.ScalarMappable(norm=norm, cmap="magma")
+cbar = g.get_figure().colorbar(sm, ax=g)
+cbar.set_label("FLOP (6ND)")
+cbar.ax.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter(useMathText=True))
+cbar.ax.yaxis.get_offset_text().set_fontsize(10)
+
+src.plot.save_plot_with_multiple_extensions(
+    plot_dir=results_dir,
+    plot_filename="y=loss_x=num_replicas_hue=flop",
+)
+plt.show()
+
+
+print("Finished 02_minerva_math_pt_qwen3.py")
