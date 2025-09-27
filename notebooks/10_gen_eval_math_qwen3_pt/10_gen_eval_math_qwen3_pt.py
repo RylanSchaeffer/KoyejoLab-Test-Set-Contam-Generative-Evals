32d98479d2c206b5a83f58110c39ec6c20edde4b
Rylan <rylanschaeffer@gmail.com>
2025-09-27 16:46:23 -0700

    Polished off notebooks/10

--- notebooks/10_gen_eval_math_qwen3_pt/10_gen_eval_math_qwen3_pt.py
+++ notebooks/10_gen_eval_math_qwen3_pt/10_gen_eval_math_qwen3_pt.py
@@ -1,8 +1,8 @@
 import ast
 
-import matplotlib.colors
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import LogNorm, SymLogNorm
 import matplotlib.pyplot as plt
-import matplotlib.ticker
 import matplotlib.transforms
 import numpy as np
 import os
@@ -15,8 +15,8 @@ import src.analyze
 import src.globals
 import src.plot
 
-# refresh = False
-refresh = True
+refresh = False
+# refresh = True
 
 data_dir, results_dir = src.analyze.setup_notebook_dir(
     notebook_dir=os.path.dirname(os.path.abspath(__file__)),
@@ -32,10 +32,12 @@ sweep_ids = [
     "1nwyun1z",  # Qwen 3  62M
     "ho49sshi",  # Qwen 3  93M
     "x8gmmzlo",  # Qwen 3  93M
+    "u5xcf726",  # Qwen 3  93M
     "sl086kx0",  # Qwen 3 153M
     "09c432gh",  # Qwen 3 344M
+    "09c432gh",  # Qwen 3 344M
     "gsx7gisg",  # Qwen 3 344M
-    "e3kneiqw",  # Qwen 3 344M
+    "6f9ah90l",  # Qwen 3 344M
 ]
 
 pretrain_run_configs_df: pd.DataFrame = src.analyze.download_wandb_project_runs_configs(
@@ -46,12 +48,17 @@ pretrain_run_configs_df: pd.DataFrame = src.analyze.download_wandb_project_runs_
     wandb_username=wandb.api.default_entity,
     finished_only=True,
 )
+
+# Extract basic quantities.
 pretrain_run_configs_df["Model"] = pretrain_run_configs_df["model_config"].apply(
     lambda model_config: ast.literal_eval(model_config)["model_name"]
 )
 pretrain_run_configs_df["Num. Parameters"] = pretrain_run_configs_df[
     "model/num_parameters"
 ]
+pretrain_run_configs_df["Parameters"] = pretrain_run_configs_df[
+    "Num. Parameters"
+].apply(lambda n: f"{n / 1_000_000:.0f}M")
 pretrain_run_configs_df["Benchmark Subset Fraction"] = pretrain_run_configs_df[
     "data_config"
 ].apply(lambda data_config: ast.literal_eval(data_config)["benchmark_subset_fraction"])
@@ -78,10 +85,22 @@ pretrain_run_configs_df["Num. Replicas Per Epoch"] = pretrain_run_configs_df[
 pretrain_run_configs_df["Num. Epochs"] = pretrain_run_configs_df[
     "trainer_config"
 ].apply(lambda trainer_config: ast.literal_eval(trainer_config)["num_train_epochs"])
-pretrain_run_configs_df["Num. Replicas"] = (
+pretrain_run_configs_df["Num. MATH Test Set Replicas"] = (
     pretrain_run_configs_df["Num. Replicas Per Epoch"]
     * pretrain_run_configs_df["Num. Epochs"]
 )
+
+num_replicas_sym_norm = SymLogNorm(
+    linthresh=1.0,
+    vmin=pretrain_run_configs_df["Num. MATH Test Set Replicas"].min(),
+    vmax=pretrain_run_configs_df["Num. MATH Test Set Replicas"].max(),
+)
+
+num_parameters_log_norm = LogNorm(
+    vmin=pretrain_run_configs_df["Num. Parameters"].min(),
+    vmax=pretrain_run_configs_df["Num. Parameters"].max(),
+)
+
 
 # Keep only runs with Benchmark Subset Fraction == 1.0
 pretrain_run_configs_df = pretrain_run_configs_df[
@@ -94,10 +113,91 @@ pretrain_run_1xOT_configs_df = pretrain_run_configs_df[
 ]
 
 plt.close()
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(16, 6))
+ax = axes[0]
+g = sns.lineplot(
+    data=pretrain_run_1xOT_configs_df,
+    x="Num. MATH Test Set Replicas",
+    y="eval_after/eval_benchmark_loss",
+    hue="Num. Parameters",
+    hue_norm=num_parameters_log_norm,
+    palette="flare",
+    marker="o",
+    # legend="full",
+    legend=False,
+    ax=ax,
+)
+g.set(
+    xscale="symlog",
+    xlim=(-0.1, 3162),
+    yscale="log",
+    ylabel="Cross Entropy on MATH Test Set",
+)
+sm_left = ScalarMappable(cmap="flare", norm=num_parameters_log_norm)
+sm_left.set_array([])
+cbarL = fig.colorbar(
+    sm_left, ax=axes[0], label="Num. Parameters", fraction=0.05, pad=0.02
+)
+
+ax = axes[1]
+g = sns.lineplot(
+    data=pretrain_run_1xOT_configs_df,
+    x="Num. Parameters",
+    y="eval_after/eval_benchmark_loss",
+    hue="Num. MATH Test Set Replicas",
+    hue_norm=num_replicas_sym_norm,
+    palette="viridis",
+    marker="o",
+    legend=False,
+    ax=ax,
+)
+g.set(
+    xscale="log",
+    yscale="log",
+    ylabel="",
+)
+sm = ScalarMappable(cmap="viridis", norm=num_replicas_sym_norm)
+sm.set_array([])
+fig.colorbar(
+    sm, ax=axes[1], label="Num. MATH Test Set Replicas", fraction=0.05, pad=0.02
+)
+src.plot.save_plot_with_multiple_extensions(
+    plot_dir=results_dir,
+    plot_filename="y=loss_by_num_parameters_by_num_replicas",
+)
+plt.show()
+
+
+plt.close()
 plt.figure(figsize=(10, 6))
 g = sns.lineplot(
     data=pretrain_run_1xOT_configs_df,
-    x="Num. Replicas",
+    x="Num. Parameters",
+    y="eval_after/eval_benchmark_loss",
+    hue="Num. MATH Test Set Replicas",
+    hue_norm=matplotlib.colors.SymLogNorm(linthresh=1.0),
+    palette="viridis",
+    marker="o",
+    legend="full",
+)
+g.set(
+    xscale="log",
+    yscale="log",
+    ylabel="Cross Entropy on MATH Test Set",
+)
+sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
+# src.plot.format_g_legend_in_scientific_notation(g=g)
+src.plot.save_plot_with_multiple_extensions(
+    plot_dir=results_dir,
+    plot_filename="y=loss_x=num_parameters_hue=num_replicas",
+)
+# plt.show()
+
+plt.close()
+plt.figure(figsize=(10, 6))
+g = sns.lineplot(
+    data=pretrain_run_1xOT_configs_df,
+    x="Num. MATH Test Set Replicas",
     y="eval_after/eval_benchmark_loss",
     hue="FLOP (6ND)",
     hue_norm=matplotlib.colors.LogNorm(),
@@ -124,7 +224,7 @@ plt.close()
 plt.figure(figsize=(10, 6))
 g = sns.lineplot(
     data=pretrain_run_1xOT_configs_df,
-    x="Num. Replicas",
+    x="Num. MATH Test Set Replicas",
     y="eval_after/eval_benchmark_loss",
     hue="Num. Parameters",
     hue_norm=matplotlib.colors.LogNorm(),
@@ -145,7 +245,7 @@ src.plot.save_plot_with_multiple_extensions(
     plot_dir=results_dir,
     plot_filename="y=loss_x=num_replicas_hue=num_parameters",
 )
-plt.show()
+# plt.show()
 
 plt.close()
 plt.figure(figsize=(10, 6))
@@ -153,9 +253,9 @@ g = sns.lineplot(
     data=pretrain_run_1xOT_configs_df,
     x="FLOP (6ND)",
     y="eval_after/eval_benchmark_loss",
-    hue="Num. Replicas",
+    hue="Num. MATH Test Set Replicas",
     hue_norm=matplotlib.colors.SymLogNorm(linthresh=1.0),
-    palette="viridis",
+    palette="cool",
     marker="o",
     legend="full",
 )
@@ -169,31 +269,6 @@ sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
 src.plot.save_plot_with_multiple_extensions(
     plot_dir=results_dir,
     plot_filename="y=loss_x=flop_hue=num_replicas",
-)
-# plt.show()
-
-plt.close()
-plt.figure(figsize=(10, 6))
-g = sns.lineplot(
-    data=pretrain_run_1xOT_configs_df,
-    x="Num. Parameters",
-    y="eval_after/eval_benchmark_loss",
-    hue="Num. Replicas",
-    hue_norm=matplotlib.colors.SymLogNorm(linthresh=1.0),
-    palette="viridis",
-    marker="o",
-    legend="full",
-)
-g.set(
-    xscale="log",
-    yscale="log",
-    ylabel="Cross Entropy on MATH Test Set",
-)
-sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
-# src.plot.format_g_legend_in_scientific_notation(g=g)
-src.plot.save_plot_with_multiple_extensions(
-    plot_dir=results_dir,
-    plot_filename="y=loss_x=num_parameters_hue=num_replicas",
 )
 # plt.show()
 
